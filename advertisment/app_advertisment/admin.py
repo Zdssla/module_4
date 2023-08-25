a34dd6a44bf229e1fbe0808515d8e3c9d304ff90
Slava <136799263+Zdssla@users.noreply.github.com>
2023-08-25 21:04:14 +0300

    Add files via upload

---
+++ advertisment/app_advertisment/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Advertisement
+
+# Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction', 'image']
+    list_filter = ['auction', 'created_at', 'price']
+    actions = ['make_auction_as_false', 'make_auction_as_True']
+    
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_as_True(self, request, queryset):
+        queryset.update(auction=True)
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+admin.site.register(Advertisement, AdvertisementAdmin)
