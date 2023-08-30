90ad76a502d4fbc564e0ea094551ff40aa0f2361
Zdssla <zdssla@gmail.com>
2023-08-30 17:56:23 +0300

    initial commit

--- advertisment/app_auth/urls.py
+++ advertisment/app_auth/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import login_view, profile_view, logout_view
+from .views import login_view, profile_view, logout_view, register_view
+from django.contrib.auth.views import LoginView
+
+class CustomLoginView(LoginView): 
+    template_name = 'index.html' 
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('pofile/', profile_view, name='profile'),
     path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
+  
